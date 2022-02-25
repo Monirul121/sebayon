60ac52be34578682eda557a73224f3e78c65f829
monirul121 <monirulislam65361@gmail.com>
2022-02-25 20:39:28 +0600

    all file upload

---
+++ dex.php
@@ -0,0 +1,125 @@
+<?php require("body.php"); ?>
+
+<body>
+    <section class="even-section">
+        <div class="container">
+            <div class="event-title">
+                <h1>কার্যক্রম সমূহ</h1>
+                <p style="margin-top: 10px;">গরিব অসহায় মানুষের পাশে দাড়ানো, পরিবেশ রক্ষা,জরুরী রক্ত দান,অস্বচ্ছল
+                    মানুষকে
+                    স্বচ্ছল করা,প্রতিবন্ধী
+                    মানুষের পাশে দাড়ানো,প্রাকৃতিক দুর্যোগকালীন ত্রান বিতরণ, আমাদের কার্যক্রম প্রচারের মাধ্যমে মানুষের
+                    মনুষ্যত্ব জাগ্রত করা।</p>
+            </div>
+            <!-- event box flex -->
+            <div class="event-box-flex">
+                <!-- event box start -->
+                <div class="event-box">
+                    <div class="icon">
+                        <i class="fa fa-search"></i>
+                    </div>
+                    <div class="text">
+                        <h2> রক্ত দান</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, praesentium?</p>
+                        <button id="event-box-button">Details</button>
+                    </div>
+                </div>
+                <!-- event box start -->
+                <div class="event-box">
+                    <div class="icon">
+                        <i class="fa fa-search"></i>
+                    </div>
+                    <div class="text">
+                        <h2> রক্ত দান</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, praesentium?</p>
+                        <button id="event-box-button">Details</button>
+                    </div>
+                </div>
+                <!-- event box start -->
+                <div class="event-box">
+                    <div class="icon">
+                        <i class="fa fa-search"></i>
+                    </div>
+                    <div class="text">
+                        <h2> রক্ত দান</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, praesentium?</p>
+                        <button id="event-box-button">Details</button>
+                    </div>
+                </div>
+                <!-- event box start -->
+                <div class="event-box">
+                    <div class="icon">
+                        <i class="fa fa-search"></i>
+                    </div>
+                    <div class="text">
+                        <h2> রক্ত দান</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, praesentium?</p>
+                        <button id="event-box-button">Details</button>
+                    </div>
+                </div>
+            </div>
+            <!-- event box flex end -->
+        </div>
+    </section>
+
+    <section class="join-us-section">
+        <div class="container">
+            <div class="join-content">
+                <h1>যোগদিন সেবায়ন ফাউন্ডেশনে </h1>
+                <p>সেবায়ন ফাউন্ডেশন উদ্যমী যেকোনো ব্যক্তির জন্য উন্মুক্ত। অসহায়-বঞ্চিত মানুষের পাশে দাঁড়ানোর সুযোগ
+                    সবসময় হয়না; যদি এই ফাউন্ডেশনের একজন গর্বিত সদস্য হিসেবে যোগ দিতে চান আমাদের সাথে, তবে ১০০ টাকা
+                    রেজিষ্ট্রেশন ফিস দিয়ে। দল-মত-ধর্ম-বর্ণ-নির্বিশেষে যেকেউ হতে পারেন আমাদের সদস্য। সদস্য হতে চাইলে
+                    নিচের ফরমটি পূরণ করে ইমেইলে পাঠিয়ে দিন আমাদের</p>
+                <div class="join-button-group">
+                    <br>
+                    <a href="">সদস্য</a>
+                    <a href="">স্বেচ্ছাসেবি / ভলান্টিয়ার </a>
+                    <a href=""> রক্তদাতা</a>
+                </div>
+            </div>
+        </div>
+        <section class="donote">
+            <div class="container">
+                <div class="donote-title">
+                    <h1>দান করুন</h1>
+                </div>
+                <!-- donote box start -->
+                <div class="donote-box-flex">
+                    <!-- donote box start -->
+                    <div class="donote-box">
+                        <div class="icon">
+                            <img src="img/BKash-Icon-Logo.wine.svg" alt="">
+                        </div>
+                        <div class="text">
+                            <h2>বিকাশ</h2>
+                            <p>01727141494</p>
+                        </div>
+                    </div>
+                    <!-- donote box start -->
+                    <div class="donote-box">
+                        <div class="icon">
+                            <img src="img/Nagad-Logo.wine.png" alt="">
+                        </div>
+                        <div class="text">
+                            <h2>নগদ</h2>
+                            <p>01754241112</p>
+                        </div>
+                    </div>
+                    <!-- donote box start -->
+                    <div class="donote-box">
+                        <div class="icon">
+                            <img src="img/dutc.png" alt="">
+                        </div>
+                        <div class="text">
+                            <h2>রকেট</h2>
+                            <p>01975635612</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- donote box flex end -->
+            </div>
+        </section>
+    </section>
+</body>
+
+</html>
---
+++ head.php
@@ -0,0 +1,66 @@
+<?php require("body.php"); ?>
+
+<body>
+    <section id="top-bar">
+        <div class="top-bar-flex">
+            <div class="social-link">
+                <ul>
+                    <li><a id="fb" href=""><i class="fa fa-facebook"></i></a></li>
+                    <li><a href=""><i class="fa fa-google-plus"></i></a></li>
+                    <li><a href=""><i id="ti" class="fa fa-twitter"></i></a></li>
+                    <li><a id="in" href=""><i class="fa fa-linkedin"></i></a></li>
+                    <li><a id="yt" href=""><i class="fa fa-youtube"></i></a></li>
+                </ul>
+            </div>
+            <div class="contract-area">
+
+                <div class="contract-icon">
+                    <i class="fa fa-envelope"></i>
+                </div>
+
+                <div class="contract-text">
+                    <h4>Email Us</h4>
+                    <p>sebayonbdofficial@gmail.com</p>
+                </div>
+
+                <div class="contract-icon">
+                    <i class="fa fa-phone" style="font-size:30px;"></i>
+                </div>
+
+                <div class="contract-text">
+                    <h4>Number</h4>
+                    <p>+880 01754241112</p>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <section id="nav-bar">
+        <div class="nav-bar-flex">
+
+            <div class="nav-logo">
+                <img src="icon/logo.png" alt="">
+            </div>
+
+            <div class="nav-menu">
+                <ul>
+                    <li><a id="home" href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Blog</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a id="search" href=""><i class="fa fa-search"></i></a></li>
+                    <li><button>FIND BLOOD</button></li>
+                </ul>
+            </div>
+            <div class="tree-menu">
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </section>
+
+</body>
+
+</html>
